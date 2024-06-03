2f1a19a384043a5d6594bbcf0f0cb94cef10fe1c
Evdokimov Denis <analitik9@gmail.com>
2024-06-03 18:05:01 +0300

    challenge with animation

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -17,6 +17,14 @@ struct ContentView: View {
     @State private var gameOver = false
     @State private var moveInGame = 8
     
+    @State private static var tappedView = Int.max
+    private var opacityCondition: ((Int)-> Double) = { num in
+         tappedView < Int.max ? num == tappedView ? 1 : 0.25 : 1
+    }
+    private var rotationCondition: ((Int,Int)-> Double) = { num, correctAnswer in
+        tappedView < Int.max ? num == correctAnswer ? 360 : 0 : 0
+    }
+    
     var body: some View {
         
         ZStack {
@@ -43,9 +51,12 @@ struct ContentView: View {
                         Button(action: { flagTapped(num) }, label: {
                             let flagName = countries[num]
                             FlagView(flagName: flagName)
+                                .opacity(opacityCondition(num))
+                                .rotation3DEffect(.degrees(rotationCondition(num, correctAnswer)),
+                                                    axis: (x: 0.0, y: 1.0, z: 0.0))
+                               
                         })
-                        
-                        
+
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -55,8 +66,6 @@ struct ContentView: View {
             Spacer()
             Spacer()
                 Text("Ваши очки \(score)")
-//                .foregroundStyle(.white)
-//                .font(.title.bold())
                     .blueLargeText()
             }
             .padding()
@@ -78,7 +87,7 @@ struct ContentView: View {
     }
     
    private func flagTapped(_ number: Int){
-      
+       ContentView.tappedView = number
         if number == correctAnswer {
             scoreTitle = "Правильно"
             score += 1
@@ -97,11 +106,13 @@ struct ContentView: View {
     private func askQuestion(){
         countries.shuffle()
        correctAnswer = Int.random(in: 0...2)
+        ContentView.tappedView = Int.max
     }
     private func reset() {
         score = 0
         gameOver = false
         moveInGame = 8
+        ContentView.tappedView = Int.max
     }
 }
 
